68f5e4f69ea5467e0aabd0aba3afe965f8452762
michael koutoumbas <mkoutoumbas@gmail.com>
2026-03-04 18:36:59 +0000

    File changes

--- functions/deleteAllPersons.ts
+++ functions/deleteAllPersons.ts
@@ -16,12 +16,27 @@ Deno.serve(async (req) => {
       return Response.json({ success: false, error: 'Unauthorized' }, { status: 401 });
     }
 
-    // Delete ALL persons in one call
-    const r = await base44.asServiceRole.entities.Person.deleteMany({});
-    const totalDeleted = r?.deleted ?? 0;
+    // Fetch and delete all persons in batches
+    let totalDeleted = 0;
+    const fetchBatchSize = 1000;
+
+    while (true) {
+      const people = await base44.asServiceRole.entities.Person.list('created_date', fetchBatchSize, 0);
+      if (people.length === 0) break;
+
+      // Delete in parallel chunks of 50
+      const chunkSize = 50;
+      for (let i = 0; i < people.length; i += chunkSize) {
+        const chunk = people.slice(i, i + chunkSize);
+        await Promise.all(chunk.map(p => base44.asServiceRole.entities.Person.delete(p.id)));
+        totalDeleted += chunk.length;
+      }
+
+      if (people.length < fetchBatchSize) break;
+    }
 
     // Reset total_records on all datasets
-    const datasets = await base44.asServiceRole.entities.Dataset.filter({}, '-created_date', 5000, 0);
+    const datasets = await base44.asServiceRole.entities.Dataset.list('-created_date', 5000, 0);
     await Promise.all(
       datasets.filter(ds => (ds.total_records ?? 0) !== 0)
               .map(ds => base44.asServiceRole.entities.Dataset.update(ds.id, { total_records: 0 }))
