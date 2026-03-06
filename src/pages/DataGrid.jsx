c93b445d266c80bbc853488861190b94836820ac
michael koutoumbas <mkoutoumbas@gmail.com>
2026-03-06 22:33:25 +0000

    File changes

--- src/pages/DataGrid.jsx
+++ src/pages/DataGrid.jsx
@@ -1,7 +1,4 @@
 import React, { useState, useEffect, useRef, useMemo, useCallback } from 'react';
-import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-alpine.css';
 import { base44 } from '@/api/base44Client';
 import { useMutation, useQueryClient, useQuery } from '@tanstack/react-query';
 import { Button } from "@/components/ui/button";
