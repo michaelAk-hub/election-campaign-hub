e9a33e2594e411d36a6ecdab9f914cb4e0393879
michael koutoumbas <mkoutoumbas@gmail.com>
2026-03-12 11:13:01 +0000

    File changes

---
+++ src/pages/KanaliPortal.jsx
@@ -0,0 +1,11 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { createPageUrl } from '../utils';
+
+export default function KanaliPortal() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate(createPageUrl('PortalLogin'), { replace: true });
+  }, [navigate]);
+  return null;
+}
